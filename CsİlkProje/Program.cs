18a014d9f8335899a02071640581b7efb0df51b1
rusy4li <90040826+rusy4li@users.noreply.github.com>
2021-12-28 22:53:31 +0300

    Add files via upload

---
+++ CsİlkProje/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toplama_Cikarma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Toplama Ve Çıkarma Yapan C# Programı!
+
+            Console.WriteLine(">>> Hangi İşlemi Yapmak İstiyorsunuz?");
+            Console.WriteLine(">>> Toplama <-> Çıkarma");
+            string girdi = Console.ReadLine();
+            if (girdi.ToLower() == "toplama")
+            {
+                Console.WriteLine(">>> 1. Sayıyı giriniz lütfen");
+                long ilksayi = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine(">>> 2. Sayıyı giriniz lütfen");
+                long ikincisayi = Convert.ToInt64(Console.ReadLine());
+                long toplam = ilksayi + ikincisayi;
+                Console.WriteLine(">>> İşlem Sonucu: {0}", toplam);
+            }
+            else
+            {
+                Console.WriteLine(">>> 1. Sayıyı giriniz lütfen");
+                long ilksayi = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine(">>> 2. Sayıyı giriniz lütfen");
+                long ikincisayi = Convert.ToInt64(Console.ReadLine());
+                long toplam = ilksayi - ikincisayi;
+                Console.WriteLine(">>> İşlem Sonucu: {0}", toplam);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VeriTipleri/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello World Yazma
+            Console.WriteLine(">>> Hello World!"); 
+            Console.WriteLine();
+
+            // Integer Veri Tipi #Tam Sayı
+            // 2,147,483,647 ve -2,147,483,647 girilebilecek en yüksek sayılardır!
+            int sayi1;
+            sayi1 = 50000;
+
+            // Long Veri Tipi #Uzun Sayı
+            // 9,223,372,036,854 ve -9,223,372,036,854 girilebilecek en yüksek sayılardır!
+            long sayi2;
+            sayi2 = 9223372036854775807;
+
+            // Short Veri Tipi #Kısa Sayı
+            // 32767 ve -32768 girilebilecek en yüksek sayılardır!
+            short sayi3;
+            sayi3 = 32767;
+
+            // Byte Veri Tipi #Düşük yer kaplar!
+            // 0 ve 255 girilebilecek en yüksek sayılardır!
+            byte sayi4;
+            sayi4 = 255;
+
+            // Double Veri Tipi #Ondalıklı Sayı
+            double sayi5;
+            sayi5 = 10.4;
+
+            // Decimal Veri Tipi #Ondalıklı Sayı
+            decimal sayi6;
+            sayi6 = 10.4m;
+
+            // Float Veri Tipi #Ondalıklı Sayı
+            float sayi7;
+            sayi7 = 10.4F;
+
+            // Bool Veri Tipi #True veya False Girilebiliyor sadece
+            bool condition;
+            condition = true;
+
+            // Char Veri Tipi #Tek karakter girilebilir sadece 'A','a' gibi
+            char character;
+            character = 'B';
+
+            // String Veri Tipi #Karakter başıan yer kaplar!
+            string greetings;
+            greetings = "Hello!";
+
+            // Var Keywords #Girdiğiniz veri türünü değişkenİsmine atıyor!
+            var number1 = "Rusy4li";
+
+            // Veri Tipi ve Kapladığı Alan
+            // Data Type    |   Size
+            //  Integer     |   4 bytes
+            //  Long        |   8 bytes
+            //  Float       |   4 bytes
+            //  Double      |   8 bytes
+            //  Bool        |   1 bit 
+            //  Char        |   2 bytes
+            //  String      |   2 bytes #Karakter başına!
+
+            // Not:
+            // 1byte = 8 bit
+
+            // WriteLine Bölümü
+            Console.WriteLine(">>> İnt değeri {0}", sayi1);
+            Console.WriteLine(">>> Long değeri {0}", sayi2);
+            Console.WriteLine(">>> Short değeri {0}", sayi3);
+            Console.WriteLine(">>> Byte değeri {0}", sayi4);
+            Console.WriteLine(">>> Double değeri {0}", sayi5);
+            Console.WriteLine(">>> Decimal değeri {0}", sayi6);
+            Console.WriteLine(">>> Float değeri {0}", sayi7);
+            Console.WriteLine(">>> Bool değeri {0}", condition);
+            Console.WriteLine(">>> Char değeri {0}", character);
+            Console.WriteLine(">>> String değeri {0}", greetings);
+            Console.WriteLine(">>> Var değeri {0}", number1);
+            Console.WriteLine(">>> Enum değeri {0}", Days.Friday);
+
+            Console.ReadKey(); // Cmd'nin kapanmasını engelliyor!
+        }
+    }
+    enum Days // Programı daha okunabilir hale getirmek için kullanıyoruz!
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday
+    }
+}
